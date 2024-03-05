2c6fcd9f3598ecdb0f86a0990447e7e6cf10130c
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2024-03-05 05:25:43 +0100

    add review and create search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,7 @@
+export default function Search({results}) {
+	return (
+		<div className="flex justify-center w-full mb-6">
+			<input className="py-2 px-3 rouded-md bg-slate-100" type="text" placeholder="Search by city name" />
+		</div>
+	)
+}
