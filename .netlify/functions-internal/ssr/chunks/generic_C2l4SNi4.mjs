67bb980656688868aa9ab59802ab80f7e6f2a227
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2024-03-08 09:33:45 +0100

    add netlify integration

---
+++ .netlify/functions-internal/ssr/chunks/generic_C2l4SNi4.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_DjgJW5yA.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_usBHGdjq.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_Cvxup6Z5.mjs').then(n => n.a);
+
+export { page };
